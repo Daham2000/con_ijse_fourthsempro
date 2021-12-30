2b67054e8822cb698927927100e2c7b22bc9cd7b
daham2000 <dahamakalanka2000@gmail.com>
2021-12-30 08:30:53 +0530

    Pagination in get all posts

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import jsonify,make_response
+from flask import jsonify,make_response,request
 from firebase_admin import firestore
 from firebase_admin import credentials,initialize_app
 import json
@@ -8,8 +8,14 @@ import firebase_admin
 
 @app.route("/")
 def index():
-    responce = get_attraction()
-    return make_response(responce, 200)
+    return "Welcome to MAYTH server."
+
+@app.route("/posts")
+def posts():
+    limit = request.args.get('limit')
+    page = request.args.get('page')
+    responce = get_attraction(int(limit),int(page))
+    return jsonify(responce)
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -20,14 +26,20 @@ cred = credentials.Certificate("private/key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-attractions = db.collection(u'attraction').stream()
-
-print(attractions)
-
-def get_attraction():
+def get_attraction(limit,page):
+    attractions_ref = db.collection(u'attraction')
+    attractions = attractions_ref.order_by(u'Title').limit(limit*(page)).stream()
     list = []
     for doc in attractions:
+        list.append(doc)
+    print(len(list))
+    last_doc = list[(-limit)]
+    last_pop = last_doc.to_dict()[u'Title']
+    print(last_pop)
+    next_query = (attractions_ref.order_by(u'Title').start_at({
+        u'Title': last_pop
+    }).limit(limit)).stream()
+    list = []
+    for doc in next_query:
         list.append(doc.to_dict())
-    jsonStr = json.dumps(list)
-    print(jsonStr)
-    return jsonStr
+    return list
